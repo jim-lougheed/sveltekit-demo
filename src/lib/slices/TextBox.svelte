3b140df1478e2fa5a85cfe12a56c2d59f8dcef9a
Jim Lougheed <jims.codes@gmail.com>
2022-12-29 21:52:56 -0500

    Create RainbowText and TextBox components

--- src/lib/slices/TextBox.svelte
+++ src/lib/slices/TextBox.svelte
@@ -1,12 +1,36 @@
 <script>
+	import * as prismicH from '@prismicio/helpers';
 	/**
 	 * @type {any}
 	 */
 	export let slice;
+
+	const reverse = slice.slice_label === 'emoji_right';
 </script>
 
-<h2>Text Box</h2>
+<div class="layout">
+	<article class:reverse>
+		<div class="emoji">{slice.primary.emoji}</div>
+		<div class="text">
+			{@html prismicH.asHTML(slice.primary.text)}
+		</div>
+	</article>
+</div>
 
-<pre>
-    {JSON.stringify(slice, null, 2)}
-</pre>
+<style>
+	article {
+		display: flex;
+		align-items: flex-start;
+		gap: 1.2rem;
+		padding: 2rem 2rem 2rem 1.4rem;
+	}
+
+	.emoji {
+		font-size: 2rem;
+	}
+
+	.reverse {
+		flex-direction: row-reverse;
+		padding: 2rem 1.4rem 2rem 2rem;
+	}
+</style>
